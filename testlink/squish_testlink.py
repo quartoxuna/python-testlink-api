781d5410276534bdf17f0bfedb64f7047c81d3a8
Kai Borowiak <kai.borowiak@secunet.com>
2013-03-20 18:09:15 +0100

    Added workaround file for squish importing correct module

---
+++ testlink/squish_testlink.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# Squish Import Workaround
+#
+# Imports all needed objects from testlink module into one
+# single module named testlink.py
+
+import testlink.api as testlink_api
+import testlink.objects as testlink_objects
