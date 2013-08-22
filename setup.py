eec946b96741e46162d6088a0df733e562ae792b
Kai Borowiak <kai.borowiak@secunet.com>
2013-08-22 13:14:37 +0200

    Refactored setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,22 @@
 from setuptools import setup
 
 setup(
-	name="testlink-api",
+	name="testlink_api",
 	version="0.1",
 	description="Testlink API Wrapper library",
 	author="Kai Borowiak",
+	author_email="kai.borowiak@secunet.com",
 	packages=['testlink'],
-	dependency_links=[
-		'http://192.168.2.20/basket/',
+	long_description="""\
+	Provides classes and methods for
+	interfacing with Testlink XML-RPC API.
+	""",
+	classifiers=[
+		"License :: OSI Approved :: GNU General Public License (GPL)",
+		"Programming Language :: Python",
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
 	],
+	keywords='testlink api xml rpc python',
+	license='GPL',
 )
