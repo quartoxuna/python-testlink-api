b3875b31bce4f1ca25ba93f746121807aeb5cba8
Kai Borowiak <kai.borowiak@secunet.com>
2014-05-15 09:29:49 +0200

    Change Egg name

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@
 from distutils.core import setup
 
 setup(
-	name='TestlinkAPI',
+	name='testlink',
 	version='0.9',
 	description='Testlink API Wrapper library',
 	author='Kai Borowiak',
 	author_email='kai.borowiak@secunet.com',
-	url='http://sinaqs.secunet.de/pypi/simple/TestlinkAPI',
 	packages=['testlink'],
 	keywords='testlink api xmlrpc python',
 	license='GPL',
