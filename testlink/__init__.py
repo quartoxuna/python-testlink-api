af84f23806f6eb98edb144c110d584a05c682c76
Kai Borowiak <kai.borowiak@secunet.com>
2014-06-04 15:56:00 +0200

    Modify exports

--- testlink/__init__.py
+++ testlink/__init__.py
@@ -6,18 +6,20 @@
 @summary: Testlink API Wrapper
 """
 
-from api import TestlinkAPI
-from api import APIError
-from api import NotSupported
+from .api import TestlinkAPI
+from .api import APIError
+from .api import NotSupported
 
-from server import TestlinkXMLRPCServer
+from .server import TestlinkXMLRPCServer
 
-from log import tl_log as TESTLINK_LOG
+from .log import tl_log as TESTLINK_LOG
 
-from objects import Testlink
-from objects import TestProject
-from objects import TestPlan
-from objects import Build
-from objects import Platform
-from objects import TestSuite
-from objects import TestCase
+from .objects import Testlink
+from .objects import TestProject
+from .objects import TestPlan
+from .objects import Build
+from .objects import Platform
+from .objects import TestSuite
+from .objects import TestCase
+
+import parsers
