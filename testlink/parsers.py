65ffbf11ddf88d7a5ca014a29b22352f4aba809b
Kai Borowiak <kai.borowiak@secunet.com>
2014-06-30 13:51:47 +0200

    Refactored parsers

--- testlink/parsers.py
+++ testlink/parsers.py
@@ -17,7 +17,7 @@ class DefaultParser(HTMLParser.HTMLParser):
 	def feed(self,data):
 		# Replace space by hand
 		data = data.replace("&nbsp;",' ')
-		return HTMLParser.HTMLParser.unescape(self,data)
+		yield HTMLParser.HTMLParser.unescape(self,data)
 
 
 class ListParser(object):
@@ -26,25 +26,26 @@ class ListParser(object):
 	def feed(self,data):
 		soup = BeautifulSoup(data)
 		if soup.ul:
-			return self.listify(soup.ul)
+			elem = soup.ul
 		elif soup.ol:
-			return self.listify(soup.ol)
+			elem = soup.ol
 		else:
-			return data
+			yield data
+		for res in self.listify(elem):
+			yield res
 
 	def listify(self,html):
-		result = []
 		for li in html.find_all('li',recursive=False):
 			try:
 				elem = next(li.stripped_strings)
-				result.append( elem )
 				if li.find('ul'):
-					result.append( self.listify(li.find('ul')) )
+					yield (elem , self.listify(li.find('ul')),)
 				elif li.find('ol'):
-					result.append( self.listify(li.find('ol')) )
+					yield (elem , self.listify(li.find('ol')),)
+				else:
+					yield (elem,)
 			except StopIteration:
 				break
-		return result
 
 
 class SectionParser(object):
@@ -53,16 +54,11 @@ class SectionParser(object):
 	def feed(self,data):
 		soup = BeautifulSoup(data)
 		if soup.p:
-			result = []
 			for p in soup.find_all('p',recursive=False):
 				try:
 					elem = next(p.stripped_strings)
-					result.append(elem)
+					yield elem
 				except StopIteration:
 					break
-			if len(result)==1:
-				return result[0]
-			else:
-				return list(result)
 		else:
-			return data
+			yield data
