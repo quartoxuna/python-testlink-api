4375b757db15544412c580488756d1534d64e96f
Kai Borowiak <kai.borowiak@secunet.com>
2014-07-09 09:12:40 +0200

    Documentation

--- testlink/objects.py
+++ testlink/objects.py
@@ -16,20 +16,41 @@ from .api import TestlinkAPI
 from .log import tl_log as log
 from .parsers import DefaultParser
 
-# Common enumerations
 class ExecutionType:
+	"""Enumeration for execution types
+	@cvar MANUAL: Manual execution
+	@type MANUAL: int
+	@cvar AUTOMATIC: Automamtic execution
+	@type AUTOMATIC: int
+	"""
 	MANUAL = 1
 	AUTOMATIC = 2
 
 class ImportanceLevel:
+	"""Enumeration for importance levels
+	@cvar HIGH: High Importance
+	@type HIGH: int
+	@cvar MEDIUM: Medium Importance
+	@type MEDIUM: int
+	@cvar LOW: Low Importance
+	@type LOW: int
+	"""
 	HIGH = 3
 	MEDIUM = 2
 	LOW = 1
 
 class DuplicateStrategy:
+	"""Enumeration for duplicate handling strategies
+	@cvar BLOCK: Block on duplicate
+	@type BLOCK: str
+	"""
 	BLOCK = 'block'
 
 class CustomFieldDetails:
+	"""Enumeration for custom field detail options
+	@cvar VALUE_ONLY: Only the value of the custom field
+	@type VALUE_ONLY: str
+	"""
 	VALUE_ONLY = 'value'
 
 class Testlink(object):
