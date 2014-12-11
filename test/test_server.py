eb7a8af22c73f7bed58893342a16272637bceabb
Kai Borowiak <kai.borowiak@secunet.com>
2014-12-11 13:48:44 +0100

    Add server tests and fixes

---
+++ test/test_server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@author: Kai Borowiak
+@summary: TestSuite for testlink.server
+"""
+
+# IMPORTS
+import unittest
+import time
+from testlink.server import TestlinkXMLRPCServer
+from xmlrpclib import ServerProxy
+
+def dummy_callback(val):
+	return val
+
+class TestlinkXMLRPCServerTests(unittest.TestCase):
+
+	def test_rpc_call(self):
+		server = TestlinkXMLRPCServer(dummy_callback)
+		client = ServerProxy("http://127.0.0.1:8080/")
+		self.assertEqual(client.executeTestCase("SPAM!"), "SPAM!")
--- testlink/server.py
+++ testlink/server.py
@@ -7,10 +7,11 @@
 """
 
 # IMPORTS
+from threading import Thread
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from log import log
 
-class TestlinkXMLRPCServer(SimpleXMLRPCServer):
+class TestlinkXMLRPCServer(SimpleXMLRPCServer,Thread):
 	"""Testlink conform XML-RPC automation server"""
 
 	def __init__(self,callback,host='127.0.0.1',port=8080,verbose=False):
@@ -25,11 +26,12 @@ class TestlinkXMLRPCServer(SimpleXMLRPCServer):
 		@type verbose: bool
 		"""
 		SimpleXMLRPCServer.__init__(self,(host,port),logRequests=verbose)
+		Thread.__init__(self)
 		self.register_introspection_functions()
 		log.info("Starting Testlink compatible XML-RPC Server at http://%s:%d/" % (str(host),int(port)) )
 		log.info("Registering '%s' as callback" % str(callback.__name__))
 		self.register_function(callback,"executeTestCase")
-		try:
-			self.serve_forever()
-		except KeyboardInterrupt:
-			log.warning("Exiting Testlink compatible XML-RPC Server at http://%s:%d/" % (str(host),int(port)) )
+		self.start()
+
+	def run(self):
+		self.handle_request()
