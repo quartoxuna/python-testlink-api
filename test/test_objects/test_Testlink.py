30ba2e7b70199f918112e79383cc6dd42f59a10d
Kai Borowiak <kai.borowiak@secunet.com>
2015-04-13 14:15:48 +0200

    Add tests for TestlinkObject and Testlink class

--- test/test_objects/test_Testlink.py
+++ test/test_objects/test_Testlink.py
@@ -8,12 +8,13 @@
 
 # IMPORTS
 import unittest
+import inspect
 from mock import Mock, MagicMock, patch
 
-from .. import randput, randict, ServerMock
+from .. import randput, randict, generate, ServerMock
 
 from testlink.api import Testlink_XML_RPC_API
-from testlink.objects import Testlink
+from testlink.objects import Testlink, TestProject
 from testlink.exceptions import *
 from testlink.enums import *
 
@@ -54,3 +55,59 @@ class TestlinkTests(unittest.TestCase):
 		mock.return_value = "1.2.3"
 		tl = Testlink(URL,DEVKEY)
 		self.assertEquals(tl.getVersion(), "1.2.3")
+
+	def test_getTestProject(self):
+		"""'getTestProject'"""
+		# Generate some test data
+		args = randput()
+		kwargs = randict("foo","bar")
+		ret = generate(1)
+
+		# Mock the eqivalent iterator
+		tl = Testlink(URL,DEVKEY)
+		tl.iterTestProject = Mock(return_value=ret)
+
+		# Check for normalize call, because there would
+		# be no single return value otherwise
+		self.assertEquals(tl.getTestProject(name=args,**kwargs),1)
+
+		# Check correct parameter forwarding
+		tl.iterTestProject.assert_called_with(args,**kwargs)
+
+	def test_iterTestProject_ShortCut_By_Name(self):
+		"""'iterTestProject' ShortCut by Name"""
+		# Generate some test data
+		kwargs = randict("name")
+
+		# Mock the raw api calls we want to track
+		tl = Testlink(URL,DEVKEY)
+		tl._api.getTestProjectByName = Mock(return_value={})
+		tl._api.getProjects = Mock()
+
+		# Check
+		tl.iterTestProject(**kwargs).next()
+		tl._api.getTestProjectByName.assert_called_with(kwargs['name'])
+		self.assertFalse(tl._api.getProjects.called)
+
+	def test_iterTestProject(self):
+		"""'iterTestProject'"""
+		# Generate some test data
+		kwargs = randict("name","notes")
+
+		# Mock the raw api calls we want to track
+		tl = Testlink(URL,DEVKEY)
+		tl._api.getTestProjectByName = Mock()
+		tl._api.getProjects = Mock(return_value= [kwargs,randict("name","notes"),randict("name","notes")] )
+
+		# Check for generator function
+		self.assertTrue(inspect.isgeneratorfunction(tl.iterTestProject))
+
+		# Check if shortcut is avoided
+		project = tl.iterTestProject(**kwargs).next()
+		self.assertFalse(tl._api.getTestProjectByName.called)
+		tl._api.getProjects.assert_called_with()
+
+		# Check correct filtered return value
+		self.assertTrue(isinstance(project,TestProject))
+		self.assertEqual(project.name, kwargs['name'])
+		self.assertEqual(project.notes, kwargs['notes'])
---
+++ test/test_objects/test_TestlinkObject.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@author: Kai Borowiak
+@summary: TestSuite for testlink.objects.TestlinkObject
+"""
+
+# IMPORTS
+import unittest
+import inspect
+
+from testlink.objects import TestlinkObject
+
+class TestlinkObjectTests(unittest.TestCase):
+
+	def __init__(self,*args,**kwargs):
+		super(TestlinkObjectTests,self).__init__(*args,**kwargs)
+		self._testMethodDoc = "TestlinkObject: " + self._testMethodDoc
+
+	def test_methods(self):
+		"""Methods"""
+		self.assertTrue(inspect.ismethod(TestlinkObject.getTestlink))
+		self.assertTrue(inspect.ismethod(TestlinkObject.iterTestProject))
