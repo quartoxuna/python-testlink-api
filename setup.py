232f8a093a097ca72ac7960fb5bcdfadd6d0bb4a
Kai Borowiak <kai.borowiak@secunet.com>
2015-06-26 11:38:48 +0200

    Remove DocBuild from setup script

--- setup.py
+++ setup.py
@@ -1,49 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# IMPORTS
-import os
-from setuptools import find_packages
 from setuptools import setup
-from setuptools import Command
 import testlink
-
-_PACKAGES_ = find_packages()
-_APIDOC_ = os.path.join("build","docs")
-
-class BuildDoc(Command):
-	description = "builds documentation using the epydoc module"
-	user_options = []
-	def initialize_options(self):
-		from epydoc import cli as epydoc_cli
-		self.options = type("",(),epydoc_cli.OPTION_DEFAULTS)()
-		self.options.verbosity = 0
-		self.options.simple_term = True
-		self.options.include_log = False
-		self.options.dotpath = None
-		self.options.action = "html"
-		self.options.target = _APIDOC_
-		if not os.path.isdir(_APIDOC_):
-			os.makedirs(_APIDOC_)
-	def finalize_options(self):
-		pass
-	def run(self):
-		from epydoc import cli as epydoc_cli
-		epydoc_cli.main(self.options,_PACKAGES_)
 
 setup(
 	name='python-testlink-api',
 	version=testlink.__version__,
-	description='Testlink API Wrapper library',
+	description='Testlink API Wrapper Library',
 	author='Kai Borowiak',
 	author_email='info@quartoxuna.com',
 	url='https://github.com/quartoxuna/python-testlink-api',
-	packages=_PACKAGES_,
+	packages=['testlink'],
 	keywords='testlink api xmlrpc python',
 
 	tests_require=['mock'],
 	test_suite='test',
-
-	extras_require = {'apidoc': ['epydoc']},
-	cmdclass={'build_doc':BuildDoc},
 )
