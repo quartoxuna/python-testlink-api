aa61cacee193d9560edc1e7ed1a66edd83d3f034
Kai Borowiak <kai.borowiak@secunet.com>
2016-11-03 12:07:38 +0100

    Pylinted setup file

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+Setup Script for python-testlink-api
+"""
+
 from setuptools import setup
 import testlink
 
-setup(
-	name='python-testlink-api',
-	version=testlink.__version__,
-	description='Testlink API Wrapper Library',
-	author='Kai Borowiak',
-	author_email='info@quartoxuna.com',
-	url='https://github.com/quartoxuna/python-testlink-api',
-	packages=['testlink'],
-	keywords='testlink api xmlrpc python',
+setup(\
+  name='python-testlink-api',\
+  version=testlink.__version__,\
+  description='Testlink API Wrapper Library',\
+  author='Kai Borowiak',\
+  author_email='info@quartoxuna.com',\
+  url='https://github.com/quartoxuna/python-testlink-api',\
+  packages=['testlink'],\
+  keywords='testlink api xmlrpc python',\
 
-	tests_require=['mock'],
-	test_suite='test',
+  tests_require=['mock'],\
+  test_suite='test'\
 )
