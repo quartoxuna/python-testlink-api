862777131cf89aa59a1dfa439cd946684401dbf1
Kai Borowiak <kai.borowiak@secunet.com>
2017-05-11 09:31:58 +0200

    Add parent TestPlan to Platforms

--- testlink/objects/tl_platform.py
+++ testlink/objects/tl_platform.py
@@ -12,12 +12,13 @@ class Platform(TestlinkObject):
     @type notes: str
     """
 
-    __slots__ = ("notes", "_parent_testproject")
+    __slots__ = ("notes", "_parent_testproject", "_parent_testplan")
 
-    def __init__(self, name=None, notes=None, parent_testproject=None, api=None, **kwargs):
+    def __init__(self, name=None, notes=None, parent_testproject=None, parent_testplan=None, api=None, **kwargs):
         TestlinkObject.__init__(self, kwargs.get('id'), name, api)
         self.notes = unicode(notes)
         self._parent_testproject = parent_testproject
+        self._parent_testplan = parent_testplan
 
     def __str__(self):
         """Returns string representation"""
@@ -30,3 +31,11 @@ class Platform(TestlinkObject):
     def iterTestProject(self):
         """Returns associated TestProject"""
         yield self._parent_testproject
+
+    def getTestPlan(self):
+        """Returns associated TestPlan"""
+        return self._parent_testplan
+
+    def iterTestPlan(self):
+        """Returns associated TestPlan"""
+        yield self._parent_testplan
