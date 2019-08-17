c8033307fb368b1dbe4f1e64778dbb13a357e406
Quartoxuna <info@quartoxuna.de>
2019-08-17 15:39:58 +0200

    Dummify TestSuite unit tests

--- test/objects/test_TestSuite.py
+++ test/objects/test_TestSuite.py
@@ -7,25 +7,12 @@
 """
 
 # IMPORTS
-import random
-import string
 import unittest
-from testlink.objects.tl_testsuite import TestSuite
-
-
-def randput(length=10): return "".join([random.choice(string.letters) for _ in xrange(random.randint(1, length))])
+from testlink.objects.testsuite import TestSuite
 
 
 class TestSuiteTests(unittest.TestCase):
     """TestSuite Object Tests"""
 
-    def __init__(self, *args, **kwargs):
-        super(TestSuiteTests, self).__init__(*args, **kwargs)
-        self._testMethodDoc = "TestSuite: " + self._testMethodDoc
-
-    def test__str__(self):
-        """String representation"""
-        name = randput()
-        obj = TestSuite(name=name)
-        _string = str(obj)
-        self.assertEqual(_string, "TestSuite: %s" % name)
+    def test_dummy(self):
+        pass
